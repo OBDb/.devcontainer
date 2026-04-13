06f3ecd731b41db92b62413165c96966cc8b76c2
Jeff Verkoeyen <jeff@clutch.engineering>
2026-04-12 21:05:05 -0600

    Fix rsync failure on Colima volume mounts (#15)
    Adds `--omit-dir-times` to the rsync call in `copy-templates.sh` so it
    doesn't fail trying to set directory timestamps on mounted volumes that
    don't support `utime()` (e.g. Colima).
    
    Previously the `postAttachCommand` would fail with rsync exit code 23,
    aborting the entire devcontainer setup.

--- copy-templates.sh
+++ copy-templates.sh
@@ -17,7 +17,7 @@ fi
 # Copy all files from template directory to workspace
 # This will overwrite workspace files that exist in the template,
 # but leave workspace-only files untouched
-rsync -av "$TEMPLATE_DIR/" "$WORKSPACE_ROOT/"
+rsync -av --omit-dir-times "$TEMPLATE_DIR/" "$WORKSPACE_ROOT/"
 echo "Template files copied successfully"
 
 echo "Template files are in place"
